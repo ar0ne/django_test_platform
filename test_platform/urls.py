6d7f268c13767ed1df5ca2427d6553972bc59942
Vova Pupkin <CAMTbITAKOU@yandex.ru>
2015-07-14 18:03:16 +0300

    add front-end urls and views

--- test_platform/urls.py
+++ test_platform/urls.py
@@ -3,6 +3,17 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    #url(r'^$', views.IndexView.as_view(), name='index')
+    # ex: /
     url(r'^$', views.index, name='index'),
+    # ex: /subject/
+    url(r'^subject/$', views.SubjectListView.as_view(), name='subject_list'),
+    # ex: /subject/2/
+    url(r'^subject/(?P<pk>[0-9]+)/$', views.SubjectView.as_view(), name='subject'),
+    # ex: /subject/2/test/
+    url(r'^subject/(?P<subject_id>[0-9]+)/test/$', views.subject_test, name='subject_test'),
+    # ex: /subject/2/topic/
+    url(r'^subject/(?P<pk>[0-9]+)/topic/$', views.TopicListView.as_view(), name='topic_list'),
+    # ex: /subject/2/topic/3/test
+    url(r'^subject/(?P<subject_id>[0-9]+)/topic/(?P<topic_id>[0-9]+)/test/$', views.topic_test, name='topic_test'),
+
 ]
--- test_platform/views.py
+++ test_platform/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import *
 
+# landing page?
 def index(request):
-    return HttpResponse("Index")
+    return render(request, 'test_platform/index.html')
 
-class IndexView():
-    pass
+class SubjectListView(generic.ListView):
+    template_name = "test_platform/subject/list.html"
+    context_object_name = "subject_list"
+
+    def get_queryset(self):
+        return Subject.objects.all()
+
+class SubjectView(generic.DetailView):
+    template_name = "test_platform/subject/detail.html"
+    model = Subject
+
+class TopicListView(generic.ListView):
+    template_name = "test_platform/topic/list.html"
+    model = Topic
+    context_object_name = "topic_list"
+
+    def get_queryset(self):
+        return Topic.objects.filter(
+            subject_id = self.kwargs['pk']
+        )
+
+def subject_test(request, subject_id):
+    return render(request, 'test_platform/subject/test.html')
+
+def topic_test(request, subject_id, topic_id):
+    return render(request, 'test_platform/topic/test.html')
